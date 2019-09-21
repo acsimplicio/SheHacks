df5228db0519dc53362e6361d91245b095681271
Ana Simplicio <asimplicio37@gmail.com>
2019-09-21 15:39:07 -0300

    olha o front nascendo

--- SheHacks/frontend/layouts/default.vue
+++ SheHacks/frontend/layouts/default.vue
@@ -1,40 +1,16 @@
 <template>
   <v-app id="inspire">
-    <toolbar :state="layout"/>
-    <sidenav-left :state="layout"/>
-    <v-content>
-      <v-container fluid>
-        <nuxt></nuxt>
-      </v-container>
+    <v-content class="bg-dark full-height full-width">
+      <nuxt></nuxt>
     </v-content>
-    <le-footer/>
-    <v-snackbar
-      :timeout="snack.timeout"
-      :color="snack.color"
-      bottom
-      v-model="snack.visible"
-    >
-      {{snack.text}}
-      <v-btn dark flat @click.native="snack.visible = false">Close</v-btn>
-    </v-snackbar>
   </v-app>
 </template>
 
 <script>
-  import toolbar from '~/components/toolbar.vue'
-  import sidenavLeft from '~/components/sidenav-left.vue'
-  import sidenavRight from '~/components/sidenav-right.vue'
-  import footer from '~/components/footer.vue'
   export default {
-    components: {
-      toolbar,
-      sidenavLeft,
-      sidenavRight,
-      leFooter: footer
-    },
     data: () => ({
       layout: {
-        drawer: true,
+        dark: true
       },
     }),
     computed: {
@@ -44,3 +20,18 @@
     }
   }
 </script>
+
+<style>
+  .full-height {
+   height: 100%;
+ }
+ .full-width {
+   width: 100%;
+ }
+ .bg-dark {
+   background-color: #252525;
+ }
+ .bg-light {
+   background-color: f3f3f3;
+ }
+</style>
--- SheHacks/frontend/pages/index.vue
+++ SheHacks/frontend/pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <v-layout justify-center align-center>
-    <v-btn color="success" :to="{name: 'todos'}">What TODO next?</v-btn>
+  <v-layout class="full-height" column justify-center align-center>
+    <v-layout class="title-container" row align-center justify-center>
+      <img src="../static/logo-white.png" height="45px" width="40px"/>
+      <div style="padding-left:6px;">Aleteia</div>
+    </v-layout>
+    <v-layout style="max-width:80%;">
+      <v-text-field class="search-box" solo></v-text-field>
+      <v-btn class="search-button">Buscar</v-btn>
+    </v-layout>
+    <v-layout class="news-container">
+
+    </v-layout>
   </v-layout>
 </template>
 
@@ -14,4 +24,32 @@ export default {
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css?family=Oldenburg&display=swap');
+  .full-height {
+    height: 100%;
+  }
+  .title-container {
+    max-height: 80px;
+    font-family: 'Oldenburg', cursive;
+    color: #f3f3f3;
+    font-size: 24px;
+    margin-top: 25px;
+  }
+  .search-box {
+    width: 350px;
+    max-height: 30px;
+    margin: 0;
+    border-radius: 0;
+  }
+  .search-button {
+    margin:0;
+    border-radius:0;
+    height: 46px;
+    color: #252525;
+    background-color: #bfbfbf;
+    text-transform: capitalize;
+  }
+  .news-container {
+    background-color: #e5e5e5;
+  }
 </style>
